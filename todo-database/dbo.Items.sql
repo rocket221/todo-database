350c194992956a9ec56b0432774b1a01b93ddc8c
Rocket <grzegorz.bury@hotmail.com>
2025-05-22 01:58:25 -0300

    changed id type

--- todo-database/dbo.Items.sql
+++ todo-database/dbo.Items.sql
@@ -1,11 +1,11 @@
 ﻿CREATE TABLE [dbo].[Items]
 (
-	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
     [Title] NVARCHAR(255) NOT NULL, 
     [Description] NVARCHAR(255) NULL, 
     [CreatedDate] DATE NOT NULL, 
-    [UserId] UNIQUEIDENTIFIER NOT NULL, 
+    [UserId] INT NOT NULL, 
     [ClosedDate] DATE NULL, 
-    [SheetId] UNIQUEIDENTIFIER NOT NULL, 
+    [SheetId] INT NOT NULL, 
     CONSTRAINT [FK_Items_Sheets] FOREIGN KEY ([SheetId]) REFERENCES [Sheets]([Id]) 
 )
